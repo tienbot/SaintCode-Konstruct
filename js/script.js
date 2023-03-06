8428436d6e093f284eabcc2519ef4a9378510661
tienbot1 <tien.ru+1@mail.ru>
2023-03-06 22:38:13 +0900

    js display header

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let body = document.querySelector('body');
+let scroll = document.querySelector('#scroll');
+let home1 = document.querySelector('#link_to_home1');
+let home2 = document.querySelector('#link_to_home2');
+
+// body.style.overflow = 'hidden';
+
+scroll.onclick = function() {
+    body.style.overflow = null;
+    document.querySelector('header').style.display = "none";
+};
+
+home1.onclick = function() {
+    document.querySelector('header').style.display = "block";
+};
+
+home2.onclick = function() {
+    document.querySelector('header').style.display = "block";
+};
